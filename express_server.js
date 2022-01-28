5cae932c069ff8b043aa97a0e773414e754a9ec4
Jomel Edodollon <jomeledodollon@yahoo.ca>
2022-01-27 21:30:42 -0800

    Implemented logout functionality

--- express_server.js
+++ express_server.js
@@ -4,17 +4,32 @@ const generateRandomString = () => {
 };
 
 const express = require("express");
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
 const app = express();
 const PORT = 8080;
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(cookieParser());
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+// Login
+app.post("/login", (req, res) => {
+  const username = req.body.username;
+  res.cookie("username", username);
+  res.redirect("/urls");
+});
+
+// Logout
+app.post("/logout", (req, res) => {
+  res.clearCookie("username");
+  res.redirect("/urls");
+});
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -29,18 +44,25 @@ app.get("/hello", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
-  console.log(templateVars);
+  const templateVars = { 
+    username: req.cookies["username"],
+    urls: urlDatabase
+  };
+  console.log(req.cookies);
   res.render("urls_index", templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const templateVars = { 
+    username: req.cookies["username"],
+  };
+  res.render("urls_new", templateVars);
 })
 
 app.get("/urls/:shortURL", (req, res) => {
   const shortURL = req.params.shortURL;
-  const templateVars = { 
+  const templateVars = {
+    username: req.cookies["username"], 
     shortURL: req.params['shortURL'],
     longURL: urlDatabase[shortURL]
   };
